67ac08fb147d391c9a6c3262e851c51a88d09c9e
Documenter.jl <documenter@juliadocs.github.io>
2022-04-04 15:14:45 +0000

    build based on 31848fc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Progradio","category":"page"},{"location":"#Progradio","page":"Home","title":"Progradio","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Progradio.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Progradio]","category":"page"}]
+}
