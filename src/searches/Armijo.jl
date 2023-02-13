156ad685d894d789d78c8d4b9b03753673a0315c
astroEduardo <72969764+astroEduardo@users.noreply.github.com>
2023-02-13 16:39:45 +0000

    Implemented improve! for Armijo box search

--- src/searches/Armijo.jl
+++ src/searches/Armijo.jl
@@ -11,8 +11,8 @@ struct Armijo{F, I} <: ProgradioSearch{F, I}
         if !(zero(F) < β < one(F))
             throw(DomainError(β, "Ensure that 0 < β < 1"))
         end
-        if !(k_0 ≥ k_min)
-            throw(DomainError(k_0, "Ensure that k_0 ≥ k_min"))
+        if !(k_0 > k_min)
+            throw(DomainError(k_0, "Ensure that k_0 > k_min"))
         end
         return new{F, I}(η_A, β, k_0, k_min)
     end
@@ -23,10 +23,12 @@ mutable struct ArmijoState{F, I} <: ProgradioSearchState{F, I}
     k::I
     α::F
     x_trial::Vector{F}
+    α_2::F
+    ϕα_2::F
 end
 
 function search_state(problem::ProgradioProblem{F, I}, search::Armijo{F, I}) where {F<:AbstractFloat, I<:Integer}
-    return ArmijoState(zero(F), search.k_0, zero(F), zeros(F, length(problem.x_0)))
+    return ArmijoState(zero(F), search.k_0, zero(F), zeros(F, length(problem.x_0)), zero(F), zero(F))
 end
 
 function initiate(problem::UProblem{F, I}, direction::ProgradioDirection{F, I}, search::Armijo{F, I}) where 
@@ -56,14 +58,14 @@ function iterate!(state::IteratorState{F, I, DS, SS}, problem::UProblem{F, I}, d
     memorize!(state);
 
     # Compute a descent direction
-    direction!(state, direction);
+    direction!(state, problem, direction);
 
     # Search for an Armijo step
     state.search_state.dϕ0 = dot(state.gx, state.direction_state.d);
     search!(state, problem, search);
 
     # Seek improved step via interpolation
-    #improve!(state, up, optimizer);
+    #improve!(state, problem, search);
 
     # Compute gradient
     problem.g!(state.gx, state.x);
@@ -89,6 +91,10 @@ function search!(state::IteratorState{F, I, DS, SS}, problem::ProgradioProblem{F
     if !admissible
         # ... until an admissible step is found
         while !admissible
+            # Keep previous α and ϕ(α) for interpolation
+            state.search_state.α_2 = state.search_state.α;
+            state.search_state.ϕα_2 = state.fx;
+
             # Take trial step
             state.search_state.k += 1;
             state.search_state.α, state.fx, admissible = trial_step!(state, problem, search, state.search_state.k, state.x);
@@ -102,13 +108,17 @@ function search!(state::IteratorState{F, I, DS, SS}, problem::ProgradioProblem{F
             if k - 1 < search.k_min
                 break
             end
-            
+
             # Take trial step
             k -= 1;
             α_trial, fx_trial, admissible = trial_step!(state, problem, search, k, state.search_state.x_trial);
 
             # Stop if trial step becomes non-admissible
             if !admissible
+                # Keep trial α and ϕ(α) for interpolation
+                state.search_state.α_2 = α_trial;
+                state.search_state.ϕα_2 = fx_trial;
+                
                 break
             end
             
@@ -140,6 +150,46 @@ function is_Armijo(state::IteratorState{F, I, DS, SS}, α::F, ϕα::F, η_A::F) 
     {F<:AbstractFloat, I<:Integer, DS<:ProgradioDirectionState{F, I}, SS<:ProgradioSearchState{F, I}}
     
     return ϕα - state.fx_previous ≤ η_A * α * state.search_state.dϕ0
+end
+#=
+function improve!(state::IteratorState{F, I, DS, SS}, problem::ProgradioProblem{F, I}, search::Armijo{F, I}) where 
+    {F<:AbstractFloat, I<:Integer, DS<:ProgradioDirectionState{F, I}, SS<:ProgradioSearchState{F, I}}
+
+    # Interpolate a new α using the two previous line-search iterations
+    α_trial = interpolate(state.fx_previous, state.search_state.α, state.fx, state.search_state.α_2, state.search_state.ϕα_2);
+
+    # Check if bounded
+    if 0 < α_trial < search.β ^ search.k_min
+        
+        # Compute x_trial
+        if α_trial ≥ state.search_state.α
+            @. state.search_state.x_trial = state.x_previous + α_trial * state.direction_state.d;
+        else
+            for j in eachindex(state.search_state.x_trial)
+                if state.W[j]
+                    state.search_state.x_trial[j] = state.x_previous[j] + α_trial * state.direction_state.d[j];
+                else
+                    state.search_state.x_trial[j] = state.x_previous[j] + state.search_state.α * state.direction_state.d[j];
+                end
+            end
+        end
+
+        # Project and check for improvement in f
+        project!(state.search_state.x_trial, problem.ℓ, problem.u);
+        fx_trial = problem.f(state.search_state.x_trial);
+        if fx_trial < state.fx
+            @. state.x = state.search_state.x_trial;
+            state.fx = fx_trial;
+        end
+    end
+
+    return nothing
+end
+=#
+function interpolate(ϕ0::F, α_1::F, ϕ_1::F, α_2::F, ϕ_2::F) where {F<:AbstractFloat}
+    a = (α_1 * (ϕ0 - ϕ_2) + α_2 * (ϕ_1 - ϕ_2)) / (α_1 * α_2 * (α_1 - α_2));
+    b = (ϕ_1 - ϕ0) / α_1 - a * α_1;
+    return -0.5 * b / a
 end
 
 # Pretty printing
--- src/searches/ArmijoBox.jl
+++ src/searches/ArmijoBox.jl
@@ -59,14 +59,14 @@ function iterate!(state::IteratorState{F, I, DS, SS}, problem::BCProblem{F, I}, 
     memorize!(state);
 
     # Compute a descent direction
-    direction!(state, direction);
+    direction!(state, problem, direction);
     
     # Projected search for an Armijo-like step
     state.search_state.dϕ0 = dot(state.gx, state.direction_state.d, state.W);
     search!(state, problem, search);
     
     # Seek improved step via interpolation
-    #improve!(state, problem, optimizer);
+    improve!(state, problem, search);
     
     # Compute gradient
     problem.g!(state.gx, state.x);
@@ -100,4 +100,38 @@ function is_Armijo(state::IteratorState{F, I, DS, SS}, α::F, ϕα::F, η_A::F, 
     {F<:AbstractFloat, I<:Integer, DS<:ProgradioDirectionState{F, I}, SS<:ProgradioSearchState{F, I}}
     
     return ϕα - state.fx_previous ≤ η_A * (α * state.search_state.dϕ0 - Δϕ0_B)
+end
+
+function improve!(state::IteratorState{F, I, DS, SS}, problem::BCProblem{F, I}, search::Armijo{F, I}) where 
+    {F<:AbstractFloat, I<:Integer, DS<:ProgradioDirectionState{F, I}, SS<:ProgradioSearchState{F, I}}
+
+    # Interpolate a new α using the two previous line-search iterations
+    α_trial = interpolate(state.fx_previous, state.search_state.α, state.fx, state.search_state.α_2, state.search_state.ϕα_2);
+
+    # Check if bounded
+    if 0 < α_trial < search.β ^ search.k_min
+        
+        # Compute x_trial
+        if α_trial ≥ state.search_state.α
+            @. state.search_state.x_trial = state.x_previous + α_trial * state.direction_state.d;
+        else
+            for j in eachindex(state.search_state.x_trial)
+                if state.W[j]
+                    state.search_state.x_trial[j] = state.x_previous[j] + α_trial * state.direction_state.d[j];
+                else
+                    state.search_state.x_trial[j] = state.x_previous[j] + state.search_state.α * state.direction_state.d[j];
+                end
+            end
+        end
+
+        # Project and check for improvement in f
+        project!(state.search_state.x_trial, problem.ℓ, problem.u);
+        fx_trial = problem.f(state.search_state.x_trial);
+        if fx_trial < state.fx
+            @. state.x = state.search_state.x_trial;
+            state.fx = fx_trial;
+        end
+    end
+
+    return nothing
 end
