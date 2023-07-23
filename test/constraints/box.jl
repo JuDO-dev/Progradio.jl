ca7ecaf782902a6f024d22d581c3ae3e5be76f9b
Jad W <jad.wehbeh@mail.mcgill.ca>
2023-07-23 20:51:11 +0100

    Add tests for kink distance computation

--- test/constraints/box.jl
+++ test/constraints/box.jl
@@ -7,4 +7,10 @@
     x = zeros(4);
     P.project!(x, box);
     @test x ≈ [-0.5, 0.5, 0.0, 0.0];
+
+    x = [-1.0, 0.75, 0.0, -1.0];
+    d = [0.0, 1.0, 1.0, -1.0];
+    κ = zeros(4);
+    P.kinkDist!(κ, x, d, box)
+    @test κ ≈ [Inf, 0.25, 0.0, Inf] 
 end
